377c2ae84cd10fe5dc5804d1833eff26085b5b30
MathoAvito <97397896+MathoAvito@users.noreply.github.com>
2024-02-24 18:01:30 +0000

    improved readability

--- macOS/homeBrewInit.sh
+++ macOS/homeBrewInit.sh
@@ -2,27 +2,40 @@
 
 set -e
 
-# Update brew
-echo "Updating brew..."
-brew update
+# Start script execution
+echo "Starting setup..."
 
-# Tap necessary repositories
+# Update Homebrew
+echo "Updating Homebrew..."
+brew update
+echo "Homebrew update completed!"
+
+# Tap repositories
 echo "Tapping necessary repositories..."
 brew tap homebrew/cask-fonts
 brew tap microsoft/git
+echo "Repositories tapped!"
 
-# Install packages
-echo "Installing packages..."
-brew install git azure-cli python@3.10 awscli bash coreutils gnutls cask curl docker gnu-sed grep helm htop jq kafka kubernetes-cli kubectx minikube mysql-client pipenv pyenv pyenv-virtualenv terraform-docs terraform-ls tfenv tflint tig tmux tree vault watch wget yq clipy rectangle
+# Install core packages
+echo "Installing core packages..."
+brew install git azure-cli python@3.10 awscli bash coreutils \
+             gnutls cask curl docker gnu-sed grep helm htop jq kafka \
+             kubernetes-cli kubectx minikube mysql-client pipenv pyenv \
+             pyenv-virtualenv terraform-docs terraform-ls tfenv tflint tig \
+             tmux tree vault watch wget yq clipy rectangle
+echo "Core packages installed!"
 
 # Install fonts
 echo "Installing fonts..."
-brew install --cask font-hack-nerd-font font-meslo-for-powerline font-meslo-lg-nerd-font
+brew install --cask font-hack-nerd-font font-meslo-for-powerline \
+                   font-meslo-lg-nerd-font
+echo "Fonts installed!"
 
-# Install additional applications
-echo "Installing applications..."
-brew install --cask git-credential-manager-core google-chrome iterm2 rectangle spotify visual-studio-code
+# Install applications
+echo "Installing additional applications..."
+brew install --cask git-credential-manager-core google-chrome iterm2 \
+                    rectangle spotify visual-studio-code
+echo "Additional applications installed!"
 
 echo "Installation completed successfully!"
-
-# Add here zshrc and ohmyzsh 
+# You can add configuration for zsh and oh-my-zsh below this line
